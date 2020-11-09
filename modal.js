a1235eda7655b8c8ff630e8e6213e5caf5fae30e
hyesungoh <haesungoh414@gmail.com>
2020-11-09 17:59:34 +0900

    add memo localStorage

--- modal.js
+++ modal.js
@@ -2,27 +2,49 @@ const buttonDiv = document.querySelector(".modal_button"),
     openBtn = buttonDiv.querySelector("button"),
     modal = document.querySelector(".modal"),
     modalContent = modal.querySelector(".modal_content"),
-    closeBtn = modal.querySelector("button");
+    closeBtn = modal.querySelector("button"),
+    textarea = modal.querySelector("textarea");
+    
 
-const HIDDEN = "hidden";
 const HIDDEN_PER = "0%";
 const SHOW_PER = "110%";
 
+const MEMO_LS = "memo";
+
+function handleTextSubmit(event){
+    if (event.keyCode == 13) {
+        if (!event.shiftKey) {
+            event.preventDefault();
+            
+            const text = textarea.value;
+            localStorage.setItem(MEMO_LS, text);
+        }
+    }
+}
+
 function handleCloseModal() {
-    // modal.classList.add(HIDDEN);
     modalContent.style.right = HIDDEN_PER;
 }
 
 function handleOpenModal() {
-    // modal.classList.remove(HIDDEN);
-    console.log("C");
     modalContent.style.right = SHOW_PER;
     closeBtn.addEventListener("click", handleCloseModal);
 
 }
 
+function loadMemo(){
+    const memo = localStorage.getItem(MEMO_LS);
+    if (memo === null) {
+
+    } else {
+        textarea.value = memo;
+    }
+}
+
 function init(){
+    loadMemo();
     openBtn.addEventListener("click", handleOpenModal);
+    textarea.addEventListener("keydown", handleTextSubmit);
 }
 
 init();
