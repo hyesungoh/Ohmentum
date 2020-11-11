e2f4345960c2b069dc5f7fa281a11363abacf439
hyesungoh <haesungoh414@gmail.com>
2020-11-11 18:57:25 +0900

    add slide random bg

--- bg.js
+++ bg.js
@@ -1,33 +1,52 @@
-const body = document.querySelector("body");
-
+const body = document.querySelector("body"),
+      bg = body.querySelector(".bg");
 const IMG_NUMBER = 7;
+const BG_SHOWING_CN = "bg_showing";
 
 
 // function handleImgLoad(){
 //   console.log("img loaded")
 // }
 
-function paintImg(imgNumber){
-  const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
-  image.classList.add("bgImage");
-  body.appendChild(image);
+function slideBg() {
+  const currentBg = bg.querySelector(`.${BG_SHOWING_CN}`);
 
-  // api 호출 시 loadend 이벤트 사용 가능
-  // image.addEventListener("loadend", handleImgLoad);
+  const randomNumber = genRandom();
+  const randomBg = bg.querySelector(`#bg_${randomNumber}`);
+  
+  if (currentBg === null){
+    randomBg.classList.add(BG_SHOWING_CN);
+  } else {
+    currentBg.classList.remove(BG_SHOWING_CN);
+    randomBg.classList.add(BG_SHOWING_CN);
+  }
+  
 }
+
+// function paintImg(imgNumber){
+//   const image = new Image();
+//   image.src = `images/${imgNumber + 1}.jpg`;
+//   image.classList.add("bgImage");
+//   body.appendChild(image);
+
+//   // api 호출 시 loadend 이벤트 사용 가능
+//   // image.addEventListener("loadend", handleImgLoad);
+// }
 
 function genRandom(){
   // Math.random() * 5 = 0~5까지 랜덤으로
   // Math.floor = 나머지 버림
   // Math.ceil = 나머지 올림
-  const number = Math.floor(Math.random() * IMG_NUMBER);
+  const number = Math.ceil(Math.random() * IMG_NUMBER);
   return number;
 }
 
+function handleClick() {
+  slideBg();
+}
 function init(){
-  const randomNumber = genRandom();
-  paintImg(randomNumber);
+  slideBg();
+  bg.addEventListener("click", handleClick);
 }
 
-init()
+init();
